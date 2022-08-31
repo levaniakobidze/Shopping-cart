8251b6be212bd8c396317b22ee7005aff91c810e
Levan Iakobidze <levaniakobidze25@gmail.com>
2022-08-31 08:34:57 -0700

    add swiper js for swiping banners

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Card from "../../components/Card/Card";
 import classes from "./Home.module.css";
 import { images } from "./homeImgs";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay, Navigation } from "swiper";
 
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
+SwiperCore.use([Autoplay, Navigation]);
 function Home() {
   const [imgIndex, setImgIndex] = useState(0);
+  const navPrevRef = useRef(null);
+  const navNextRef = useRef(null);
 
   const incrementImgIndex = () => {
     if (imgIndex > images.length - 2) {
@@ -42,11 +51,45 @@ function Home() {
     <section className={classes.home}>
       <Card>
         <div className={classes.home_main_img}>
-          <img src={images[imgIndex]} alt='' />
+          {/* <img src={images[imgIndex]} alt='' />
           <div className={classes.img_btns}>
             <button onClick={decrementImgIndex}>{"<"}</button>
             <button onClick={incrementImgIndex}>{">"}</button>
-          </div>
+          </div> */}
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            loop={true}
+            modules={[Autoplay, Navigation]}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+            }}
+            centeredSlides
+            pagination={{ clickable: true }}
+            navigation={{
+              clickable: true,
+              prevEl: navPrevRef.current,
+              nextEl: navNextRef.current,
+            }}>
+            <SwiperSlide>
+              <img src={images[0]} alt='' />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={images[1]} alt='' />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={images[2]} alt='' />
+            </SwiperSlide>
+            <div className={classes.img_btns}>
+              <button ref={navPrevRef} className={classes.prev}>
+                {"<"}
+              </button>
+              <button ref={navNextRef} className={classes.next}>
+                {">"}
+              </button>
+            </div>
+          </Swiper>
         </div>
       </Card>
     </section>
